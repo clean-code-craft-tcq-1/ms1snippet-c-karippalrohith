69db2b324ad58b3314b6b0acb46328c0b6a11927
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-22 16:43:15 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -7,20 +7,10 @@ int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
   return 1;
 }
 
-int validateSOCreadings(double* values, int numOfValues) {
+int validateParameter_Readings(double* values, int numOfValues, double deviationValue) {
   int lastButOneIndex = numOfValues - 1;
   for(int i = 0; i < lastButOneIndex; i++) {
     if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return 0;
-    }
-  }
-  return 1;
-}
-
-int validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
       return 0;
     }
   }
