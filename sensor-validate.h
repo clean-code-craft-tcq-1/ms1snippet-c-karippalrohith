4289ad1e114ba6ca966c69ac7ba9a4535ff6fb42
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-22 17:38:09 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,4 +1,33 @@
-int _give_me_a_good_name(double value, double nextValue, double maxDelta);
+/* ******************************************************************************
+ ***     \file        sensor-validate.h
+ ***     \ingroup     Evaluation-TCQ
+ ***     \author      Rohith K N
+ ***
+ ***     \brief       Header file for the module
+ ***
+ ***     \par         File_description
+ ***                  This file have the declarations of the variables and methods of the module
+ ***
+ *****************************************************************************/
+#ifndef SENSOR_VALIDATION_H_
+#define SENSOR_VALIDATION_H_
+/*=============================================================================
+ =======                            INCLUDES                             =======
+ ============================================================================*/
+/*------ standard includes -------*/
 
-int validateParameter_Readings(double* values, int numOfValues, double deviationValue);
+/*------ module includes -------*/
 
+typedef enum {	DEVIATION_IN_PARAMETER_VALUE, 
+				NO_DEVIATION_IN_PARAMETER_VALUE
+}parameter_Deviation_t;
+
+typedef enum {	INVALID_PARAMETER_VALUES, 
+				VALID_PARAMETER_VALUES
+}parameter_Values_Validity_t;
+
+parameter_Deviation_t validate_Deviation_In_Consecutive_Parameter_Values(double value, double nextValue, double maxDelta);
+
+parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int numOfValues, double deviationThreshold);
+
+#endif /*SENSOR_VALIDATION_H_*/
