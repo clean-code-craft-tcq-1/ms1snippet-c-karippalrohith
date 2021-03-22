520e8436bf8bfff460942afcd94a96f4761f0f21
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-22 17:37:42 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -1,18 +1,72 @@
+/* ******************************************************************************
+ ***
+ ***     \file        Checker.c
+ ***     \ingroup     Session2-TCQ
+ ***     \author      Rohith K N
+ ***
+ ***     \brief       Main file to check BMS
+ ***
+ ***     \par         File_description
+ ***                  This file explores more about functions
+ ***
+ *****************************************************************************/
+ 
+/*=============================================================================
+ =======                            INCLUDES                             =======
+ ============================================================================*/
+/*------ standard includes -------*/
+
+/*------ module includes --------*/
 #include "sensor-validate.h"
 
-int _give_me_a_good_name(double value, double nextValue, double maxDelta) {
-  if(nextValue - value > maxDelta) {
-    return 0;
+/*=============================================================================
+ =======               DEFINES & MACROS FOR GENERAL PURPOSE              =======
+ =============================================================================*/
+
+/*=============================================================================
+ =======                       CONSTANTS  &  TYPES                       =======
+ =============================================================================*/
+
+/*=============================================================================
+ =======                VARIABLES & MESSAGES & RESSOURCEN                =======
+ ==============================================================================*/
+
+/*=============================================================================
+ =======                              METHODS                            =======
+ =============================================================================*/
+
+
+parameter_Deviation_t validate_Deviation_In_Consecutive_Parameter_Values(double value, double nextValue, double maxDelta) {
+  
+  if((nextValue - value) > maxDelta) 
+  {
+    return DEVIATION_IN_PARAMETER_VALUE;
   }
-  return 1;
+  return NO_DEVIATION_IN_PARAMETER_VALUE;
 }
 
-int validateParameter_Readings(double* values, int numOfValues, double deviationValue) {
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION: validateParameter_Readings
+ */
+/*!    \brief   Function to validate the parameters
+ *
+ *
+ *     \param    	paramValues -> Sensor values for the respective parameter
+					numOfValues -> Number of samples
+					deviationThreshold -> the threshold for which sensor value can vary wrt to the previous value
+ *     \returns  	void
+ *
+ *//*------------------------------------------------------------------------*/
+parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int numOfValues, double deviationThreshold) {
+	
   int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], deviationValue)) {
-      return 0;
+  
+  for(int i = 0; i < lastButOneIndex; i++) 
+  {
+    if(!validate_Deviation_In_Consecutive_Parameter_Values(paramValues[i], paramValues[i + 1], deviationValue)) 
+	{
+      return INVALID_PARAMETER_VALUES;
     }
   }
-  return 1;
+  return VALID_PARAMETER_VALUES;
 }
