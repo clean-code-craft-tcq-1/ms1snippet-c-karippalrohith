5a5590c0755220624ce44ca35709981990f43678
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-23 10:28:22 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -74,16 +74,14 @@ parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int 
 	{		
 		parameter_Deviation_Check = validate_Deviation_In_Consecutive_Parameter_Values(paramValues[i], paramValues[i + 1], deviationThreshold);
 		
-		if(parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE) /*Check for if any parameter value have a deviation*/
-		{
-			return INVALID_PARAMETER_VALUES;
-		}
+		(parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE)?break:continue;
 	}
   //}
   //else
   //{
 	//parameter_Values_Validity_Status = INVALID_PARAMETER_VALUES;
   //}
-  
-  return parameter_Values_Validity_Status;
+	parameter_Values_Validity_Status = (parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE)?INVALID_PARAMETER_VALUES:VALID_PARAMETER_VALUES;
+	
+	return parameter_Values_Validity_Status;
 }
