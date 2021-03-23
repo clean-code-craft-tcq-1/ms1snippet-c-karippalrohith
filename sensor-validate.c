6611d6a6b72548cdfb726874a7ab0911afd6eeef
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-23 10:22:28 +0530

    Update sensor-validate.c

--- sensor-validate.c
+++ sensor-validate.c
@@ -64,10 +64,9 @@ parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int 
 	
   int lastButOneIndex = numOfValues - 1;
   parameter_Deviation_t parameter_Deviation_Check;
-  parameter_Values_Validity_t parameter_Values_Validity_Status;
-  int retVal = 1;
+  parameter_Values_Validity_t parameter_Values_Validity_Status = VALID_PARAMETER_VALUES;
   
-  retVal = (NULL == paramValues)?0:1;
+  parameter_Values_Validity_Status = (NULL == paramValues)?INVALID_PARAMETER_VALUES:VALID_PARAMETER_VALUES;
   
   //if((NULL != paramValues))
   //{
@@ -75,18 +74,16 @@ parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int 
 	{		
 		parameter_Deviation_Check = validate_Deviation_In_Consecutive_Parameter_Values(paramValues[i], paramValues[i + 1], deviationThreshold);
 		
-		//if(parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE) /*Check for if any parameter value have a deviation*/
-		//{
-		//	return INVALID_PARAMETER_VALUES;
-		//}
-		
-		retVal &= ((parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE)?0:1);
+		if(parameter_Deviation_Check == DEVIATION_IN_PARAMETER_VALUE) /*Check for if any parameter value have a deviation*/
+		{
+			return INVALID_PARAMETER_VALUES;
+		}
 	}
   //}
   //else
   //{
 	//parameter_Values_Validity_Status = INVALID_PARAMETER_VALUES;
   //}
-	parameter_Values_Validity_Status = (retVal == 0)?INVALID_PARAMETER_VALUES:VALID_PARAMETER_VALUES;
+  
   return parameter_Values_Validity_Status;
 }
