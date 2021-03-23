fc21d7c0de5dcf8e1a602c76c3999fc75cc38d40
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-03-23 10:41:00 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -31,6 +31,6 @@ typedef enum {	INVALID_PARAMETER_VALUES,
 
 parameter_Deviation_t validate_Deviation_In_Consecutive_Parameter_Values(double value, double nextValue, double maxDelta);
 
-parameter_Values_Validity_t validateParameter_Readings(double* paramValues, int numOfValues, double deviationThreshold);
+int validateParameter_Readings(double* paramValues, int numOfValues, double deviationThreshold);
 
 #endif /*SENSOR_VALIDATION_H_*/
